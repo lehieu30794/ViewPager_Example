1cf0be97c9a96a4f4af04d13d82f2db66556050d
lehieu30794 <lehieu30794@gmail.com>
2018-07-23 20:50:00 -0600

    Add TabLayout, will do visual polish next

--- app/src/main/java/com/example/android/viewpager/MondayFragment.java
+++ app/src/main/java/com/example/android/viewpager/MondayFragment.java
@@ -15,11 +15,11 @@
  */
 package com.example.android.viewpager;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
 
 /**
  * Fragment that displays "Monday".
@@ -27,7 +27,10 @@ import android.view.ViewGroup;
 public class MondayFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_monday, container, false);
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_monday,
+                container, false);
     }
 }
